595416c0d5417b705debecb10c183f14e8d1379e
Neo-Diamons <romain.hure@epitech.eu>
2024-03-13 11:29:19 +0100

    doc(key): invalid path and missing doc for class Texture.

--- include/Texture.hpp
+++ include/Texture.hpp
@@ -13,18 +13,38 @@
 #include "Color.hpp"
 
 namespace arc {
-
+    /**
+     * @brief Class for the texture which is used to handle the textures with graphical libraries change at runtime
+     */
     class Texture {
         std::string _path;
         std::string _pattern;
         Color _color;
 
     public:
+        /**
+         * @brief Constructor for the texture
+         * @param path Path to the texture, use by graphical libraries (like SFML, SDL, etc.)
+         * @param pattern Pattern of the texture, use by textual libraries (like Ncurses, etc.)
+         * @param color Color of the texture
+         */
         Texture(const std::string &path, const std::string &pattern, Color color)
             : _path(path), _pattern(pattern), _color(color) {}
 
+        /**
+         * @brief Get the path of the texture
+         * @return The path of the texture
+         */
         [[nodiscard]] std::string GetPath() const { return _path; }
+        /**
+         * @brief Get the pattern of the texture
+         * @return The pattern of the texture
+         */
         [[nodiscard]] std::string GetPattern() const { return _pattern; }
+        /**
+         * @brief Get the color of the texture
+         * @return The color of the texture
+         */
         [[nodiscard]] Color GetColor() const { return _color; }
     };
 }

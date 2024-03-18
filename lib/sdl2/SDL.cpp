ac621fd33e28aeb342aa1e770904055ccf9173fd
mlargeot <morgan.largeot@epitech.eu>
2024-03-18 09:27:52 +0100

    feat(SDL): Add base for SDL class

---
+++ lib/sdl2/SDL.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2024
+** Arcade
+** File description:
+** SDL
+*/
+
+#include "SDL.hpp"
+
+void arc::SDL::init(){}
+void arc::SDL::stop(){}
+void arc::SDL::clear(){}
+void arc::SDL::display(){}

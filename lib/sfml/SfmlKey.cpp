f440af59e43a33ef3de0b36e2d230fcb9964243c
fry <j.danet@arka-studio.fr>
2024-03-29 09:32:33 +0100

    fix(SFML): Reset key press

--- lib/sfml/SfmlKey.cpp
+++ lib/sfml/SfmlKey.cpp
@@ -6,13 +6,19 @@
 */
 
 #include "SfmlKey.hpp"
+#include <iostream>
 
 void arc::SfmlKey::setKeyPressed(sf::Keyboard::Key key)
 {
+    std::cout << "Key pressed: " << key << std::endl;
     _lastKeyPressed = key;
 }
 
 bool arc::SfmlKey::isKeyPressed(KeyEnum key)
 {
     return _keyMap.find(key)->second == _lastKeyPressed;
+}
+
+void arc::SfmlKey::resetKey() {
+    _lastKeyPressed = UNKNOWN;
 }
